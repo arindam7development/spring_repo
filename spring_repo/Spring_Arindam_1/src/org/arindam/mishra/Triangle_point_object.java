4f62d61c1c93c49ad4b913432feccdc5fe44d093
Arindam Mishra <arindam5191@gmail.com>
2014-09-03 23:59:25 +0530

    Update Triangle_point_object.java

--- spring_repo/Spring_Arindam_1/src/org/arindam/mishra/Triangle_point_object.java
+++ spring_repo/Spring_Arindam_1/src/org/arindam/mishra/Triangle_point_object.java
@@ -3,17 +3,32 @@
  */
 package org.arindam.mishra;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 /**
  * @author ARINDAM
  *
  */
-public class Triangle_point_object {
+public class Triangle_point_object implements ApplicationContextAware,BeanNameAware {
 	
 	private Point pointA;
 	private Point pointB;
 	private Point pointC;
+	private ApplicationContext context =null;
 	
-	
+	public ApplicationContext getContext() {
+		return context;
+	}
+
+
+	public void setContext(ApplicationContext context) {
+		this.context = context;
+	}
+
+
 	public Point getPointA() {
 		return pointA;
 	}
@@ -49,6 +64,23 @@ public class Triangle_point_object {
 		System.out.println("Point A ("+getPointA().getX()+","+getPointA().getY()+")");
 		System.out.println("Point B ("+getPointB().getX()+","+getPointB().getY()+")");
 		System.out.println("Point C ("+getPointC().getX()+","+getPointC().getY()+")");
+		
+	}
+
+
+	@Override
+	public void setBeanName(String beanName) {
+		// TODO Auto-generated method stub
+		System.out.println("Bean Name"+beanName);
+	}
+
+
+	@Override
+	public void setApplicationContext(ApplicationContext context)
+			throws BeansException {
+		// TODO Auto-generated method stub
+		this.context =context ;
+		
 		
 	}
 	
