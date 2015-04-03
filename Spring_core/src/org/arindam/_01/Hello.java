c01a84ef15362199e0d600fdf2a55de8022395f7
Arindam Mishra <arindam5191@gmail.com>
2015-04-03 16:37:33 +0530

    Create Hello.java
    This is a simple hello world example with different variations

---
+++ Spring_core/src/org/arindam/_01/Hello.java
@@ -0,0 +1,47 @@
+package java_rest;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/hello")
+public class Hello {
+
+  // This method is called if TEXT_PLAIN is request
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String sayPlainTextHello() {
+    return "Hello Jersey";
+  }
+
+  // This method is called if XML is request
+  @GET
+  @Produces(MediaType.TEXT_XML)
+  public String sayXMLHello() {
+    return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+  }
+
+  // This method is called if HTML is request
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  public String sayHtmlHello() {
+    return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+        + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+  }
+  
+  @GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+
+		String output = "Jersey say : " + msg;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+} 
